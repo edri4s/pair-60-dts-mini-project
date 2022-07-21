cd48766fb441b1f4e91644dd1b892c6da40b848b
edri4s <edri.adisetiawan@gmail.com>
2022-07-21 14:57:44 +0800

    tampilan awal

--- src/App.js
+++ src/App.js
@@ -1,17 +1,59 @@
 //import logo from './logo.svg'; 
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
 import Home from './components/Home';
 
+const POPULER_API = "https://api.themoviedb.org/3/movie/popular?api_key=eac2fb034a00cbefd16d1b4689d2543a&language=en-US";
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?api_key=eac2fb034a00cbefd16d1b4689d2543a&query=";
+
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    getMovies(POPULER_API);
+  }, [])
+
+  const getMovies = (API) => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setMovies(data.results);
+      });
+  }
+
+  const handlerOnSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      getMovies(SEARCH_API + searchTerm)
+      setSearchTerm('');
+    }
+  }
+
+  const handlerOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <Routes>
-        <Route path='/home' component={Home} />
-        <Route path='/login' />
-        <Route path='/register' />
-      </Routes>
-    </div>
+    <>
+      <header>
+        <span className='identity'>PAIR-60 MOVIE</span>
+
+        <span>
+        </span>
+        <form onSubmit={handlerOnSubmit}>
+          <input className='search' type="search" placeholder='Search...' value={searchTerm} onChange={handlerOnChange} />
+        </form>
+      </header>
+
+      <div className="movie-container">
+        {movies.length > 0 && movies.map(movie => (
+          <Home key={movie.id} {...movie} />
+        ))}
+      </div>
+    </>
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,30 @@
-import { Paper } from "@mui/material";
-import { Container } from "@mui/system";
-import React, { components } from "react";
+import React from "react";
 
-export default class Home extends components {
-    render() {
-        return (
-            <Container>
-                <Paper>
-                    <h1>Ini Home</h1>
-                </Paper>
-            </Container>
-        )
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+
+const voteClass = (vote) => {
+    if (vote >= 8) {
+        return 'green';
+    } else if (vote >= 6) {
+        return 'orange';
+    } else {
+        return 'red';
     }
 }
+
+const Home = ({ title, poster_path, overview, vote_average }) => (
+    <div className="movie">
+        <img src={poster_path ? IMG_API + poster_path : "https://images.unsplash.com/photo-1626814026160-2237a95fc5a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"} alt={title} />
+        <div className="movie-info">
+            <h4 className="title">{title}</h4>
+            <span className={`tagVote ${voteClass(vote_average)}`}>{vote_average}</span>
+        </div>
+
+        <div className="movie-over">
+            <h2>Overview</h2>
+            <p>{overview}</p>
+        </div>
+    </div>
+);
+
+export default Home;
